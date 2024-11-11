df611c3cc8c2e036b1290c05ce32386cc1f3733d
EdSabhin <edsabhin@yahoo.com>
2024-11-11 10:49:25 -0500

    Added login and register pages and routes.

---
+++ src/app/register/page.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <div>
+        <h1>Login</h1>
+        <div>
+          <input type="text" />
+          <input type="text" />
+        </div>
+        <div>
+        <div>
+          <input type="checkbox" />
+          <span>Remember Me</span>
+          </div>
+          <span>Forget me</span>
+        </div>
+        <button>Login</button>
+      </div>
+    </div>
+  )
+}
+
+export default page
